751270793a1ef1304c3c253a808d2734410c6368
jhonjpg <jonathan050315jj@gmail.com>
2025-05-13 19:06:35 -0400

    name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-    base: "sharkWeb",
+    base: "sharkweb",
     plugins: [react()]
 })
