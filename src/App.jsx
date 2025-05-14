ab428005494905fabfc3b246517868442e3e0ab6
jhonjpg <jonathan050315jj@gmail.com>
2025-05-13 20:09:01 -0400

    sh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import { Home } from './components/Home'
 import { About } from './components/About'
 import { Species } from './components/Species'
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-    base: "sharkweb",
-    plugins: [react()]
+    base: '/sharkweb/', // <-- SLASH al inicio y al final
+    plugins: [react()],
 })
